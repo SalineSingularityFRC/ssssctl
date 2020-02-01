9cff3312ac188b95dc692787b73903ac58cf0e14
Skye Bleed <monarrk@aaathats3as.com>
2020-01-31 19:17:13 -0500

    Send all the data this time

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,10 @@ mod err;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::io::{Read, Write};
 use err::handle;
 use err::err;
 use bluetooth_serial_port::{BtProtocol, BtSocket, BtDevice, BtAddr};
-use mio::{Poll, PollOpt, Ready, Token};
-use std::{
-    io::{Read, Write},
-};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,45 +21,38 @@ fn main() {
 
     // create and connect the RFCOMM socket
     let mut socket = handle(BtSocket::new(BtProtocol::RFCOMM), "Failed to create new bluetooth socket");
+    match socket.connect(device.addr) {
+        Ok(_) => {},
+        Err(why) => {
+            println!("Failed to connect to {}: {}", device.addr.to_string(), why);
+            std::process::exit(1);
+        }
+    };
 
-    handle(socket.connect(device.addr), format!("Failed to connect to {}", device.addr.to_string()).as_str());
-
-    let bytes = handle(fs::read(Path::new(&args[3])), "Error reading file");
+    // Read the file to bytes
+    let bytes = handle(fs::read(Path::new(&args[3])), format!("Error reading file {}", &args[3]).as_str());
     
     // Data buf for IO
     let mut buffer = [0; 10];
 
     // Read and write data over the connection
-    let num_bytes_written = match socket.write(&bytes[..]) {
+    let num_bytes_written = match socket.write_all(&bytes[0..]) {
         Ok(e) => e,
         Err(why) => {
             eprintln!("Error writing bytes over socket: {}", why);
             std::process::exit(1);
         }
     };
+    socket.write(b"EOF").expect("Failed to write to socket");
 
+    // Read data from connection 
     let num_bytes_read = match socket.read(&mut buffer[..]) {
         Ok(e) => e,
         // If this fails, don't panic, just return 0 and print an err
         Err(why) => {
-            eprintln!("Failed to read from socket; continuing {}", why); 0
+            eprintln!("Failed to read from socket ({}); continuing", why); 0
         }
     };
 
-    println!("Read {} bytes, wrote {} bytes", num_bytes_read, num_bytes_written);
-
-    // Asnyc IO for BtSocket 
-    let poll = Poll::new().expect("Failed to create poll");
-    match poll.register(
-        &socket,
-        Token(0),
-        Ready::readable() | Ready::writable(),
-        PollOpt::edge() | PollOpt::oneshot(),
-    ) {
-        Ok(_) => {},
-        Err(why) => {
-            eprintln!("Failed to register: {}", why);
-            std::process::exit(1);
-        }
-    };
+    println!("Read {} bytes, wrote {:?} bytes", num_bytes_read, num_bytes_written);
 }
